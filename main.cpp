8a4b200fa550a011a89c4ac0b97209dca57fca7b
Nehal Jhajharia <76217999+nehalJhajharia@users.noreply.github.com>
2021-11-08 06:47:00 +0530

    Update main.cpp
    Root of cubic is precise instead of a range.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,27 @@
 using namespace std;
 
 #define ESC 27
-#define DT 0.1
+#define DT 100
 
 // Essential variables
 char key ('\0');
-float a (0);
-float b (0);
-float c (0);
-float d (0);
+float a (0); // Coefficient
+float b (0); // Coefficient
+float c (0); // Coefficient
+float d (0); // Coefficient
 float D (0); // Discriminent
 float A (0); // Root 1
 float B (0); // Root 2
 float C (0); // Root 3
 int complexCheck (0); // whether Discriminent is (-)ve
+double xp(0); // (+)ve x
+double xn(0); // (-)ve x
+int xp_check (1); // (+)ve x
+int xn_check (1); // (-)ve x
+float p = DT; // variable for root precision in cubic equation
+double Fp (0); // function value at (+)ve x
+double Fn (0); // function value at (-)ve x
+int decPrecion (1);
 
 // Header file of some user-defined functions
 #include "Functions.h"
@@ -37,226 +45,19 @@ int main()
         {
         case '1': // Linear Equation
             separate('*');
-            // taking input
-            cout << "For ax + b = 0,\n";
-            cout << "a : ";
-            cin >> a;
-            cout << "b : ";
-            cin >> b;
-
-            // Check if the equation is strictly linear
-            if (a == 0) // No Equation
-            {
-                cout << "No equation to solve!!";
-                separate('*');
-                break;
-            }
-
-            // display equation
-            cout << "\n";
-            if (a != 1 && a != -1)
-                cout << a;
-            if (a == -1)
-                cout << "-";
-            cout << "x";
-            if (b != 0)
-            {
-                if (b < 0)
-                    cout << " - " << (-1)*b;
-                else
-                    cout << " + " << b;
-            }
-            cout << " = 0\n\n";
-
-            // solving equation
-            b *= -1;
-            A = b / a;
-            cout << "x = " << A << "\n";
+            Linear();
             separate('*');
             break;
 
         case '2': // Quadratic Equation
             separate('*');
-            // taking input
-            cout << "For ax2 + bx + c = 0,\n";
-            cout << "a : ";
-            cin >> a;
-            cout << "b : ";
-            cin >> b;
-            cout << "c : ";
-            cin >> c;
-
-            // Check if the equation is strictly quadratic
-            if (a == 0) // Linear Equation
-            {
-                if (c == 0) // No equation
-                {
-                    cout << "No equation to solve!!";
-                    separate('*');
-                    break;
-                }
-                cout << "It's a linear equation, try '1' command!!\n";
-                separate('*');
-                break;
-            }
-
-            // display equation
-            cout << "\n";
-            if (a != 1 && a != -1)
-                cout << a;
-            if (a == -1)
-                cout << "-";
-            cout << "(x2)";
-            if (b != 0)
-            {
-                if (b < 0)
-                {
-                    cout << " - ";
-                    if (b != -1)
-                        cout << (-1)*b;
-                }
-                else
-                {
-                    cout << " + ";
-                    if (b != 1)
-                        cout << b;
-                }
-                cout << "x";
-            }
-            if (c != 0)
-            {
-                if (c < 0)
-                    cout << " - " << (-1)*c;
-                else
-                    cout << " + " << c;
-            }
-            cout << " = 0\n\n";
-
-            // solving discriminent
-            D = (b*b) - 4*a*c;
-            if (D < 0) // Complex roots
-            {
-                D *= -1;
-                complexCheck = 1;
-            }
-
-            D = sqrt(D);
-
-            b *= -1;
-            a *= 2;
-            b /= a;
-            D /= a;
-
-            if (complexCheck == 1) // Complex roots
-            {
-                if (b != 0)
-                {
-                    cout << "Root 1 = " << b << " + " << D << "i\n";
-                    cout << "Root 2 = " << b << " - " << D << "i\n";
-                }
-                else
-                {
-                    cout << "Root 1 = " << D << "i\n";
-                    cout << "Root 2 = " << "-" << D << "i\n";
-                }
-                separate('*');
-                break;
-            }
-
-            A = b + D;
-            B = b - D;
-
-            cout << "Root 1 = " << A << "\n";
-            cout << "Root 2 = " << B << "\n";
+            Quadratic();
             separate('*');
             break;
 
         case '3': // Cubic Equation
             separate('*');
-            // taking input
-            cout << "For ax3 + bx2 + cx + d = 0,\n";
-            cout << "a : ";
-            cin >> a;
-            cout << "b : ";
-            cin >> b;
-            cout << "c : ";
-            cin >> c;
-            cout << "d : ";
-            cin >> d;
-
-            // Check if the equation is strictly cubic
-            if (a == 0) // Quadratic Equation
-            {
-                if (b == 0) // Linear equation
-                {
-                    if (c == 0) // No equation
-                    {
-                        cout << "No equation to solve!!\n";
-                        separate('*');
-                        break;
-                    }
-                    cout << "It's a linear equation, try '1' command!!\n";
-                    separate('*');
-                    break;
-                }
-                cout << "It's a quadratic equation, try '2' command!!\n";
-                separate('*');
-                break;
-            }
-
-            // display equation
-            cout << "\n";
-            if (a != 1 && a != -1)
-                cout << a;
-            if (a == -1)
-                cout << "-";
-            cout << "(x3)";
-            if (b != 0)
-            {
-                if (b < 0)
-                {
-                    cout << " - ";
-                    if (b != -1)
-                        cout << (-1)*b;
-                }
-                else
-                {
-                    cout << " + ";
-                    if (b != 1)
-                        cout << b;
-                }
-                cout << "(x2)";
-            }
-            if (c != 0)
-            {
-                if (c < 0)
-                {
-                    cout << " - ";
-                    if (c != -1)
-                        cout << (-1)*c;
-                }
-                else
-                {
-                    cout << " + ";
-                    if (c != 1)
-                        cout << c;
-                }
-                cout << "x";
-            }
-            if (d != 0)
-            {
-                if (d < 0)
-                    cout << " - " << (-1)*d;
-                else
-                    cout << " + " << d;
-            }
-            cout << " = 0\n\n";
-
-            // SOLVE
-            if (d != 0) // 0 is not a root
-                A = alpha();
-            cout << "Root 1 belongs to [" << A-DT << ", " << A+DT << "]\n";
-
+            Cubic();
             separate('*');
             break;
 
