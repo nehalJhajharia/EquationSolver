c8f495745598e07c77945a31f0d6176844695837
Nehal Jhajharia <76217999+nehalJhajharia@users.noreply.github.com>
2021-11-11 09:25:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,24 +7,24 @@ using namespace std;
 #define DT 100
 
 // Essential variables
-char key ('\0');
-float a (0); // Coefficient
-float b (0); // Coefficient
-float c (0); // Coefficient
-float d (0); // Coefficient
-float D (0); // Discriminent
-float A (0); // Root 1
-float B (0); // Root 2
-float C (0); // Root 3
-int complexCheck (0); // whether Discriminent is (-)ve
-double xp(0); // (+)ve x
-double xn(0); // (-)ve x
-int xp_check (1); // (+)ve x
-int xn_check (1); // (-)ve x
+char key {'\0'};
+float a {0}; // Coefficient
+float b {0}; // Coefficient
+float c {0}; // Coefficient
+float d {0}; // Coefficient
+float D {0}; // Discriminent
+float A {0}; // Root 1
+float B {0}; // Root 2
+float C {0}; // Root 3
+int complexCheck {0}; // whether Discriminent is (-)ve
+double xp{0}; // (+)ve x
+double xn{0}; // (-)ve x
+int xp_check {1}; // (+)ve x
+int xn_check {1}; // (-)ve x
 float p = DT; // variable for root precision in cubic equation
-double Fp (0); // function value at (+)ve x
-double Fn (0); // function value at (-)ve x
-int decPrecion (1);
+double Fp {0}; // function value at (+)ve x
+double Fn {0}; // function value at (-)ve x
+int decPrecion {1};
 
 // Header file of some user-defined functions
 #include "Functions.h"
